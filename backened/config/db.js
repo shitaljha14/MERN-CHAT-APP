69038b3bd4efb0516f4b2b6762b67176af2eea0a
shitajha14 <shitaljha2312@gmail.com>
2024-08-17 13:50:01 +0530

    initial commit

--- backened/config/db.js
+++ backened/config/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const mongoose=require("mongoose");
 const connectDB=async() => {
     try{
-        const conn=await mongoose.connect(process.env.MONGO_URI
-           
+        const conn=await mongoose.connect(process.env.MONGO_URI,{
+           useNewUrlParser: true,
+      useUnifiedTopology: true,
             
-        );
+     } );
         console.log(`MongoDB Connected:${conn.connection.host}`);
     }catch (error){
      console.log(`Error:${error.message}`);
--- backened/server.js
+++ backened/server.js
@@ -21,3 +21,4 @@ const PORT =process.env.PORT||8000;
 app.listen(8000, 
 console.log(`Server started on PORT ${PORT}`));
 
+
